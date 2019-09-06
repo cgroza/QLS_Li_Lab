3bde104fbb52b55494b776bf3ce573ceb817992a
Cristian Groza <cristian.groza@protonmail.com>
2019-09-06 13:56:23 -0400

    Pipeline script.

--- topic_learning.R
+++ topic_learning.R
@@ -4,7 +4,13 @@ library(topicmodels)
 
 ## loading kmer documents from a directory
 dir <- "/home/mcb/li_lab/cgroza/dev/QLS_Li_Lab"
-dir.source <- DirSource(dir)
+dir.source <- DirSource(dir, pattern = "kmers")
 corpus <- SimpleCorpus(dir.source)
 kmer.matrix <- DocumentTermMatrix(corpus)
 
+## k <- 30
+## SEED <- 2010
+## jss_TM <- list(VEM = LDA(JSS_dtm, k = k, control = list(seed = SEED)),
+##                VEM_fixed = LDA(JSS_dtm, k = k, control = list(estimate.alpha = FALSE, seed = SEED)),
+##                Gibbs = LDA(JSS_dtm, k = k, method = "Gibbs", control = list(seed = SEED, burnin = 1000, thin = 100, iter = 1000)),
+##                CTM = CTM(JSS_dtm, k = k,  control = list(seed = SEED, var = list(tol = 10^-4), em = list(tol = 10^-3))))
