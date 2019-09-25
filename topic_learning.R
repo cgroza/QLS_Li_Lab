9f4de9c573bc5d1c9dca39a22c6c6f70c33b2941
Cristian Groza <cristian.groza@protonmail.com>
2019-09-25 11:48:04 -0400

    Fewer terms.

--- topic_learning.R
+++ topic_learning.R
@@ -10,11 +10,19 @@ library(purrr)
 
 ## load Kmers
 
-data <- readRDS("kmers_16_dtm.Rds")
+data <- readRDS("kmers_16_dtm_pruned.Rds")
 s<-sample(1:3484, 2000)
 train <- data[s,]
 test  <- data[-s,]
 
 k <- 7
 SEED <- 2019
-topic.model <- LDA(train, k = k, method = "Gibbs", control = list(seed = SEED, burnin = 1000, thin = 100, iter = 1000))
+topic.model <- LDA(train, k = k, method = "Gibbs", control = list(
+                                                     seed = SEED,
+                                                     burnin = 1000,
+                                                     thin = 100,
+                                                     iter = 1000,
+                                                     verbose = 10,
+                                                     save = 1,
+                                                     prefix = "LDA_"
+                                                     ))
